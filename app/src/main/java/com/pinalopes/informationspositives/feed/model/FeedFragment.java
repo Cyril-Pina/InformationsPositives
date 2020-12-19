ddca8e9840f0af16b6e19a567a69c5aa573f3f15
Cyril PL <cyril@groupay.fr>
2020-12-19 23:20:48 +0100

    + add stories front

--- app/src/main/java/com/pinalopes/informationspositives/feed/model/FeedFragment.java
+++ app/src/main/java/com/pinalopes/informationspositives/feed/model/FeedFragment.java
@@ -1,7 +1,6 @@
 package com.pinalopes.informationspositives.feed.model;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,8 +8,11 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
 
 import com.pinalopes.informationspositives.LoadingService;
+import com.pinalopes.informationspositives.R;
 import com.pinalopes.informationspositives.databinding.FeedFragmentBinding;
 import com.pinalopes.informationspositives.feed.dagger.DaggerFeedComponent;
 import com.pinalopes.informationspositives.feed.dagger.LoadingModule;
@@ -19,14 +21,11 @@ import javax.inject.Inject;
 
 public class FeedFragment extends Fragment {
 
-    private static final String TAG = "FeedFragment";
-
     @Inject LoadingService loadingService;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        Log.d(TAG, "onCreateView: ");
         FeedFragmentBinding binding = FeedFragmentBinding.inflate(inflater, container, false);
         View rootView = binding.getRoot();
         DaggerFeedComponent
@@ -34,6 +33,13 @@ public class FeedFragment extends Fragment {
                 .loadingModule(new LoadingModule(rootView.getContext(), binding.loadingMainLayout, binding.loadingImageView))
                 .build()
                 .inject(this);
+        initStoriesFragment((FragmentActivity) rootView.getContext());
         return rootView;
+    }
+
+    private void initStoriesFragment(FragmentActivity fragmentActivity) {
+        FragmentManager fragmentManager = fragmentActivity.getSupportFragmentManager();
+        Fragment mapFragment = new StoryFragment();
+        fragmentManager.beginTransaction().add(R.id.storiesFrameLayout, mapFragment).commit();
     }
 }
